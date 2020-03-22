06cdc7156358cc9bb1fe69bb7db5406b3869a7a9
unknown <bgajdkos@gmail.com>
2020-03-22 15:02:06 +0100

    feat: My first JAMstack site

---
+++ main.js
@@ -0,0 +1,22 @@
+const listRepos = async username => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+  )
+    .then(res => res.json())
+    .catch(console.error);
+
+  const markup = repos
+    .map(
+      repo => `
+    <li>  
+    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+    (&#x1F31F; &#x1F410; ${repo.stargazers_count})
+    </li>
+  `
+    )
+    .join("");
+  const content = document.getElementById("content");
+  content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos('jlengstorf');
